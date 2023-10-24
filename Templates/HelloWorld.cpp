181d829591eabfa4a24aa5b9364f815dd01dff55
kfy666 <101913445+kfy666@users.noreply.github.com>
2023-10-24 20:37:27 +0800

    Create HelloWorld.cpp

---
+++ Templates/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>//这个是头文件
+using namespace std;//使用命名空间std
+
+int main()//主函数
+{
+    printf("Hello World!\n");//请记住在需要的地方加上分号;
+    //也可使用下一行代码
+	//cout<<"Hello World!"<<endl;
+    return 0;//return 0是好习惯
+}
