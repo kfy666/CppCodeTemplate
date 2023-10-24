112dae1a2c4438f69563a6c881d1e24a7dc90032
kfy666 <101913445+kfy666@users.noreply.github.com>
2023-10-24 20:18:35 +0800

    Update Dijkstra-Leftist-tree.cpp

--- Templates/Dijkstra-Leftist-tree.cpp
+++ Templates/Dijkstra-Leftist-tree.cpp
@@ -11,15 +11,15 @@ ll n, m, s;
 struct Edge
 {
 	ll ne, to, w;
-}e[N << 1];
+}e[N << 1];//e存边
 
-void add(ll u, ll v, ll w)
+void add(ll u, ll v, ll w)//加边
 {
 	++tot;
 	e[tot].to = v, e[tot].w = w, e[tot].ne = head[u], head[u] = tot;
 }
 
-ll merge(ll x, ll y)
+ll merge(ll x, ll y)//左偏树的merge操作
 {
 	if(!x || !y) return x + y;
 	if(val[y] < val[x]) swap(x, y);
@@ -29,7 +29,7 @@ ll merge(ll x, ll y)
 	return x;
 }
 
-void dijkstra(ll s)
+void dijkstra(ll s)//最短路
 {
 	memset(dis, 0x3f, sizeof(dis));
 	dis[s] = 0;
@@ -45,11 +45,11 @@ void dijkstra(ll s)
 		if(rt == 0) break;
 		ll u = ver[rt];
 		book[u] = 1;
-		for(ll i = head[u];~i;i = e[i].ne)
+		for(ll i = head[u];~i;i = e[i].ne)//遍历u的出边, 此处用~i判断head[i]是否为-1(空)
 		{
 			ll v = e[i].to;
 			if(book[v]) continue;
-			if(dis[v] > dis[u] + e[i].w)
+			if(dis[v] > dis[u] + e[i].w)//松弛
 			{
 				dis[v] = dis[u] + e[i].w;
 				val[++cnt] = dis[v];
@@ -63,8 +63,8 @@ void dijkstra(ll s)
 
 int main()
 {
-	memset(head, -1, sizeof(head));
-	scanf("%lld%lld%lld", &n, &m, &s);
+	memset(head, -1, sizeof(head));//数组初始化为-1, 防止内存读取时出错
+	scanf("%lld%lld%lld", &n, &m, &s);//输入点数n, 边数m, 源点s
 	for(ll i = 1;i <= m;i++)
 	{
 		ll u, v, w;
@@ -72,7 +72,7 @@ int main()
 		add(u, v, w);
 	}
 	dijkstra(s);
-	for(ll i = 1;i <= n;i++)
+	for(ll i = 1;i <= n;i++)//输出各点到s的距离
 	{
 		printf("%lld ", dis[i]);
 	}
