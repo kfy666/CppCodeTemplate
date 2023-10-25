a4894e65ea0d8a1c2f5f71861cda3a58795ae8f9
kfy666 <101913445+kfy666@users.noreply.github.com>
2023-10-25 22:07:43 +0800

    Create A+B-Problem.cpp

---
+++ Templates/A+B-Problem.cpp
@@ -0,0 +1,14 @@
+//A+B Problem
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 5;
+
+int a, b;//使用全局变量, 默认值为0
+
+int main()
+{
+	scanf("%d%d", &a, &b);//输入a, b
+	printf("%d\n", a + b);//输出a + b
+	return 0;
+}
+
