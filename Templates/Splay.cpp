f891bded8bd4a47f20c70eae3450c67daa85345f
GTC-Byzantine <118645189+GTC-Byzantine@users.noreply.github.com>
2023-10-27 20:19:28 +0800

    Create Splay.cpp

---
+++ Templates/Splay.cpp
@@ -0,0 +1,181 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6 + 6;
+int rt, tot, fa[N], ch[N][2], val[N], cnt[N], sz[N];
+
+// Обновление значения узла x
+// 在改变节点位置后，将节点 x 的 size 更新
+void maintain(int x){
+	sz[x] = sz[ch[x][0]] + sz[ch[x][1]] + cnt[x];
+}
+
+// Определите, является ли узел x левым или правым потомком родительского узла
+// 判断节点 x 是父亲节点的左儿子还是右儿子
+bool get(int x){
+	return x == ch[fa[x]][1];
+}
+
+// Уничтожение узла x
+// 销毁节点 x
+void clear(int x){
+	ch[x][0] = ch[x][1] = fa[x] = val[x] = sz[x] = cnt[x] = 0;
+}
+
+// вращать
+//旋转 
+void rotate(int x){
+	int y = fa[x], z = fa[y], chk = get(x);
+	ch[y][chk] = ch[x][chk ^ 1];
+	if(ch[x][chk ^ 1])	fa[ch[x][chk ^ 1]] = y;
+	ch[x][chk ^ 1] = y;
+	fa[y] = x;
+	fa[x] = z;
+	if(z) ch[z][y == ch[z][1]] = x;
+	maintain(y);
+	maintain(x);
+}
+
+// Операции Splay
+// Splay 操作 
+void splay(int x) {
+	for(int f = fa[x]; f = fa[x], f; rotate(x))
+		if(fa[f]) rotate(get(x) == get(f) ? f : x);
+	rt = x;
+}
+
+// Операция вставки
+//插入操作 
+void ins(int k) {
+	if(!rt) {
+		val[++tot] = k;
+		cnt[tot]++;
+		rt = tot;
+		maintain(rt);
+		return;
+	}
+	int cur = rt, f = 0;
+	while(1) {
+		if (val[cur] == k) {
+		cnt[cur]++;
+		maintain(cur);
+		maintain(f);
+		splay(cur);
+		break;
+	}
+	f = cur;
+	cur = ch[cur][val[cur] < k];
+	if(!cur) {
+		val[++tot] = k;
+		cnt[tot]++;
+		fa[tot] = f;
+		ch[f][val[f] < k] = tot;
+		maintain(tot);
+		maintain(f);
+		splay(tot);
+		break;
+		}
+	}
+}
+
+// Запрос ранга x
+//查询 x 的排名
+int rk(int k) {
+	int res = 0, cur = rt;
+	while(1) {
+		if (k < val[cur]) {
+		cur = ch[cur][0];
+		}
+		else {
+			res += sz[ch[cur][0]];
+			if (k == val[cur]) {
+				splay(cur);
+				return res + 1;
+		}
+		res += cnt[cur];
+		cur = ch[cur][1];
+		}
+	}
+}
+
+// Количество запросов с рангом x
+// 查询排名为 x 的数
+int kth(int k) {
+	int cur = rt;
+	while (1) {
+		if (ch[cur][0] && k <= sz[ch[cur][0]]) {
+			cur = ch[cur][0];
+		}
+		else {
+			k -= cnt[cur] + sz[ch[cur][0]];
+			if (k <= 0) {
+				splay(cur);
+				return val[cur];
+			}
+			cur = ch[cur][1];
+		}
+	}
+}
+
+// прекурсор запроса
+// 查询前驱
+int pre() {
+	int cur = ch[rt][0];
+	if (!cur) return cur;
+	while (ch[cur][1]) cur = ch[cur][1];
+	splay(cur);
+	return cur;
+}
+
+// Последующие действия по запросу
+// 查询后继
+int nxt() {
+	int cur = ch[rt][1];
+	if (!cur) return cur;
+	while (ch[cur][0]) cur = ch[cur][0];
+	splay(cur);
+	return cur;
+}
+
+// Слияние двух деревьев
+//合并两棵树
+void del(int k) {
+	rk(k);
+	if (cnt[rt] > 1) {
+		cnt[rt]--;
+		maintain(rt);
+		return;
+	}
+	if (!ch[rt][0] && !ch[rt][1]) {
+		clear(rt);
+		rt = 0;
+		return;
+	}
+	if (!ch[rt][0]) {
+		int cur = rt;
+		rt = ch[rt][1];
+		fa[rt] = 0;
+		clear(cur);
+		return;
+	}
+	if (!ch[rt][1]) {
+		int cur = rt;
+		rt = ch[rt][0];
+		fa[rt] = 0;
+		clear(cur);
+		return;
+	}
+	int cur = rt, x = pre();
+	fa[ch[cur][1]] = x;
+	ch[x][1] = ch[cur][1];
+	clear(cur);
+	maintain(rt);
+}
+
+int main(){
+
+	return 0;
+}
+
+
+
