cae1b742ac127834ee4e900443e771ff381dd04c
kfy666 <101913445+kfy666@users.noreply.github.com>
2023-11-12 20:47:13 +0800

    Update Prime-Euler-sieve.cpp

--- Templates/Prime-Euler-sieve.cpp
+++ Templates/Prime-Euler-sieve.cpp
@@ -1,21 +1,21 @@
 //线性筛法筛质数
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const ll N = 1e5 + 5;
+typedef long long LL;
+const LL N = 1e5 + 5;
 
 bool vis[N];
-ll pri[N], cnt;
+LL pri[N], cnt;
 
-void init(ll n)//线性筛法筛质数
+void init(LL n)//线性筛法筛质数
 {
 	vis[0] = vis[1] = 1;
-	for(ll i = 2; i <= n; i++)
+	for(LL i = 2; i <= n; i++)
 	{
 		if(!vis[i]) pri[cnt++] = i;
-		for(ll j = 0; j < cnt; j++)
+		for(LL j = 0; j < cnt; j++)
 		{
-			if(1ll * i * pri[j] > n) break;
+			if(1LL * i * pri[j] > n) break;
 			vis[i * pri[j]] = 1;
 			if(i % pri[j] == 0) break;
 		}
@@ -25,9 +25,9 @@ void init(ll n)//线性筛法筛质数
 
 int main()
 {
-	ll ed = 1000000ll;//ed是筛质数的上界
+	LL ed = 1000000ll;//ed是筛质数的上界
 	init(ed);
-	for(ll i = 1; i <= min(N, ed); i++)//输出筛出的质数
+	for(LL i = 1; i <= min(N, ed); i++)//输出筛出的质数
 	{
 		if(!vis[i]) printf("%lld ", i);
 	}
