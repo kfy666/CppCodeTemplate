01542495f6839497c602bb4e54f3f798e756dc5d
kfy666 <101913445+kfy666@users.noreply.github.com>
2023-11-13 17:21:49 +0800

    Update Chinese-remainder-theorem.cpp

--- Templates/Chinese-remainder-theorem.cpp
+++ Templates/Chinese-remainder-theorem.cpp
@@ -1,39 +1,49 @@
-#include <iostream>
-#define int LL
-#define LL long long
+#include<bits/stdc++.h>
+typedef long long LL;
 using namespace std;
 
-int exgcd(int a, int b, int &x, int &y) {
-  if (!b) {
-    x = 1;
-    y = 0;
-    return a;
-  }
-  int d = exgcd(b, a % b, x, y);
-  int t = x;
-  x = y;
-  y = t - (a / b) * y;
-  return d;
+LL exgcd(LL a, LL b, LL &x, LL &y)
+{
+	if(!b)
+	{
+    	x = 1;
+    	y = 0;
+    	return a;
+  	}
+	LL d = exgcd(b, a % b, x, y);
+	LL t = x;
+	x = y;
+	y = t - (a / b) * y;
+  	return d;
 }
 
-LL mods[15], arr[15];
+LL mods[15], arr[15], n;
 
-LL CRT(int k, LL* a, LL* r) {
-  LL n = 1, ans = 0;
-  for (int i = 1; i <= k; i++) n = n * r[i];
-  for (int i = 1; i <= k; i++) {
-    LL m = n / r[i];
-    LL b, y;
-    exgcd(m, r[i], b, y);  // b * m mod r[i] = 1
-    ans = (ans + a[i] * m * b % n) % n;
-  }
-  return (ans % n + n) % n;
+LL CRT(LL k, LL* a, LL* r)
+{
+	LL n = 1, ans = 0;
+  	for(LL i = 1; i <= k; i++)
+	{
+		n = n * r[i];
+	}
+  	for(LL i = 1; i <= k; i++)
+	{
+    	LL m = n / r[i];
+    	LL b, y;
+    	exgcd(m, r[i], b, y);// b * m mod r[i] = 1
+    	ans = (ans + a[i] * m * b % n) % n;
+  	}
+  	return (ans % n + n) % n;
 }
 
 
-signed main() {
-    int n;
-    cin >> n;
-    for (int i =1 ; i <= n; i++) cin >> mods[i] >> arr[i];
-    cout << CRT(n, arr, mods);
+int main()
+{
+	scanf("%lld", &n);
+    for(int i =1 ; i <= n; i++)
+	{
+		scanf("%lld%lld", mods + i, arr + i);
+	}
+    printf("%lld\n", CRT(n, arr, mods));
+	return 0;
 }
