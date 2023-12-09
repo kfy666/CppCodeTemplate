d3396f674f431a532f008e15e633a9238e8e3780
GTC-Byzantine <118645189+GTC-Byzantine@users.noreply.github.com>
2023-12-09 08:09:24 +0800

    Create Floyd.cpp

---
+++ Templates/Graph/Floyd.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e4 + 5;
+
+int dis[N][N], n;
+
+void floyd()
+	for (int k = 1; k <= n; k++)
+		for (int x = 1; x <= n; x++)
+			for (int y = 1; y <= n; y++)
+				dis[x][y] = min(dis[x][y], dis[x][k] + dis[k][y]);
+
+int main() {
+	return 0;
+}
