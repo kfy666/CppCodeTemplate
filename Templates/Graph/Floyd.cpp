9e26a3978106998d50e86cd30d15b134dc33967f
kfy666 <101913445+kfy666@users.noreply.github.com>
2023-12-09 08:12:15 +0800

    Update Floyd.cpp

--- Templates/Graph/Floyd.cpp
+++ Templates/Graph/Floyd.cpp
@@ -1,3 +1,4 @@
+//Floyd求最短路
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 1e4 + 5;
@@ -5,11 +6,20 @@ const int N = 1e4 + 5;
 int dis[N][N], n;
 
 void floyd()
-	for (int k = 1; k <= n; k++)
-		for (int x = 1; x <= n; x++)
-			for (int y = 1; y <= n; y++)
+{
+	for(int k = 1; k <= n; k++)
+	{
+		for(int x = 1; x <= n; x++)
+		{
+			for(int y = 1; y <= n; y++)
+			{
 				dis[x][y] = min(dis[x][y], dis[x][k] + dis[k][y]);
+			}
+		}
+	}
+}
 
-int main() {
+int main()
+{
 	return 0;
 }
